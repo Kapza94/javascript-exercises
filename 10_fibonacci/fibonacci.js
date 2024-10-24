57614ed895b0c08766dcbc35cde9f61bf955fb6f
Kapza <lukakapetanovic@icloud.com>
2024-10-24 14:27:52 +0200

    fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = parseInt(num);
+  if (num <= 1) return num;
 
+  let first = 0;
+  let second = 1;
+
+  for (let i = 2; i <= num; i++) {
+    let next = first + second;
+    first = second;
+    second = next;
+  }
+
+  return second;
 };
+
+console.log("30");
 
 // Do not edit below this line
 module.exports = fibonacci;
