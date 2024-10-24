08bb8fd412ffdee3377cdcf7a1dcb70e595d6be9
Kapza <lukakapetanovic@icloud.com>
2024-10-24 13:49:54 +0200

    Palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  //have to check for case sensitivity.
+  str = str.toLowerCase();
+  str = str.replace(/[^a-zA-Z0-9]/g, "");
 
+  //splits str
+  const splitStr = str.split("");
+
+  const reverseStr = splitStr.reverse("");
+
+  const joinStr = reverseStr.join("");
+
+  if (str === joinStr) {
+    console.log("This is a Palindrome");
+    return "This is a Palindrome";
+  } else {
+    return "This is NOT a Palindrome";
+  }
 };
+
+console.log(palindromes("DAD"));
 
 // Do not edit below this line
 module.exports = palindromes;
